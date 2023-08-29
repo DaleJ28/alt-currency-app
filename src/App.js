54c4765956781d314df8f1a55f2ef471f591e75d
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-08-29 18:49:02 -0400

    added 404 file and router path alt-currency-app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
-
 import './App.css';
 
 const App = () => {
   return (
-    <Router>
+    <Router basename="/alt-currency-app">
       <Layout>
         <Switch>
           <Route path="/" exact render={() => <h1>Hello world</h1>} />
