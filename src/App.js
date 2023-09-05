289e504125e96fcadb3ceb9a3dd6a1ca53785864
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-09-05 19:38:27 -0400

    removed unwanted dependencies

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
 import Home from './Home';
-import './App.css';
 
 const App = () => {
   return (
