af2368999d652b8464573b91afe37f265aabb907
Dale Jiblal <70293940+DaleJ28@users.noreply.github.com>
2023-09-05 17:21:48 -0400

    merge to gh-pages

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Layout from './Layout';
+import Home from './Home';
 import './App.css';
 
 const App = () => {
@@ -9,7 +10,7 @@ const App = () => {
     <Router basename="/alt-currency-app">
       <Layout>
         <Switch>
-          <Route path="/" exact render={() => <h1>Hello world</h1>} />
+          <Route path="/" exact render={(Home) => <h1>Hello world</h1>} />
           <Route render={() => <h1>404 Not found</h1>} />
         </Switch>
       </Layout>
